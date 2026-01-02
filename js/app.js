6791d55bccdcab81aa1f26618e2464d4249865a9
Build New Habits <buildnewhabits@outlook.com>
2026-01-02 07:55:39 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -510,6 +510,7 @@ window.alongside = {
   celebrate,
   skipToday: todayView.skipToday,
   selectWorkout: todayView.selectWorkout,
+  startWorkout: todayView.startWorkout
   resetApp,
   // Savings functions
   logSaving: savingsTracker.logSaving,
